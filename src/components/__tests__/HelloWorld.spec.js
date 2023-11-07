c0d3437fe8fc9f281b602f5f24b5ac6a4e1e6600
AravinthAdarsh <111167525+AravinthAdarsh@users.noreply.github.com>
2023-11-07 16:09:51 +0800

    first commit

---
+++ src/components/__tests__/HelloWorld.spec.js
@@ -0,0 +1,43 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import HelloWorld from '../HelloWorld.vue'
+
+import RandomNumber from "../RandomNumber.vue"
+
+describe('Hello World', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, {
+      props: { msg: "Hello Vitest" }
+    })
+    expect(wrapper.text()).toContain("Hello Vitest")
+  })
+})
+
+
+describe("RandomNumber.vue", () => {
+  it("By default the random number should be 0", () => {
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain('0')
+  }),
+
+  it("if button clicked, the random number should be from 1-10", async() => {
+    const wrapper = mount(RandomNumber)
+    await wrapper.find('button').trigger('click')
+    const random_number = parseInt(wrapper.find('h2').text())
+    expect(random_number).toBeGreaterThanOrEqual(0)
+    expect(random_number).toBeLessThanOrEqual(10)
+  }),
+
+  it("the number follows the props min max", async() => {
+    const wrapper = mount(RandomNumber,{
+      props: {
+        min:40, max:100
+      }
+    })
+    await wrapper.find('button').trigger('click')
+    const random_number = parseInt(wrapper.find('h2').text())
+    expect(random_number).toBeGreaterThanOrEqual(40)
+    expect(random_number).toBeLessThanOrEqual(100)
+  })
+})
